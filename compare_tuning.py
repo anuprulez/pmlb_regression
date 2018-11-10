bb65778d0efdcfb10fca13cec128c9640a26b5f2
Anup Kumar <anup.rulez@gmail.com>
2018-11-10 23:24:45 +0100

    Update plot scripts

---
+++ compare_tuning.py
@@ -0,0 +1,187 @@
+import sys
+import time
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib import rcParams
+import os
+import numpy as np
+import warnings
+
+import plotly
+import plotly.graph_objs as go
+from plotly import tools
+import plotly.io as pio
+
+
+warnings.filterwarnings('ignore')
+
+start_time = time.time()
+folder_name_tuning = "tuning/"
+folder_name_no_tuning = "no_tuning/"
+clf_names_dir_tuning = os.listdir(folder_name_tuning)
+clf_names_dir_no_tuning = os.listdir(folder_name_no_tuning)
+
+
+def check_file(f_name):
+    for clf in clf_names_dir_tuning:
+        data_path = folder_name_tuning + clf
+        files = [f for f in os.listdir(data_path)]
+        if f_name not in files:
+            return False
+            break
+    return True
+
+
+clf_names_tuning = list()
+clf_mean_r2_tuning = list()
+r2_dict_tuning = dict()
+fit_time_dict_tuning = dict()
+
+print("Processing results for tuning ...")
+
+for clf in clf_names_dir_tuning:
+    data_path = folder_name_tuning + clf
+    files = [f for f in os.listdir(data_path)]
+    r2_scores = list()
+    fit_time = list()
+    common_fileset = list()
+    for f_name in files:
+        if_file_present = check_file(f_name)
+        if if_file_present is True:
+            common_fileset.append(f_name)
+            f_path = data_path + "/" + f_name
+            df = pd.read_csv(f_path, sep="\t")
+            rank_test_score = df[df["rank_test_score"] == 1]
+            mean_test_score = rank_test_score["mean_test_score"]
+            fit_time_sec = rank_test_score["mean_fit_time"].iloc[0] + rank_test_score["mean_score_time"].iloc[0]
+            r2_scores.append(mean_test_score.iloc[0])
+            fit_time.append(fit_time_sec)
+    clf_names_tuning.append(clf)
+    mean_r2 = np.mean(r2_scores)
+    clf_mean_r2_tuning.append(mean_r2)
+    r2_dict_tuning[clf] = mean_r2
+    mean_fit_time = np.mean(fit_time)
+    fit_time_dict_tuning[clf] = mean_fit_time
+    print("Mean R2 score for %s regressor: %0.2f" % (clf, mean_r2))
+    print("Total fit time for %s regressor: %0.4f seconds" % (clf, mean_fit_time))
+
+    print("-------------x------------------x---------------x---------------")
+    print(" ")
+
+
+clf_names_no_tuning = list()
+clf_mean_r2_no_tuning = list()
+r2_dict_no_tuning = dict()
+fit_time_dict_no_tuning = dict()
+
+print("Processing results for no tuning ...")
+
+for clf in clf_names_dir_no_tuning:
+    data_path = folder_name_no_tuning + clf
+    files = [f for f in os.listdir(data_path)]
+    r2_scores = list()
+    fit_time = list()
+    for f_name in files:
+        if_file_present = check_file(f_name)
+        if if_file_present is True:
+            f_path = data_path + "/" + f_name
+            df = pd.read_csv(f_path, sep="\t")
+            rank_test_score = df[df["rank_test_score"] == 1]
+            mean_test_score = rank_test_score["mean_test_score"]
+            fit_time_sec = rank_test_score["mean_fit_time"].iloc[0] + rank_test_score["mean_score_time"].iloc[0]
+            r2_scores.append(mean_test_score.iloc[0])
+            fit_time.append(fit_time_sec)
+    clf_names_no_tuning.append(clf)
+    mean_r2 = np.mean(r2_scores)
+    clf_mean_r2_no_tuning.append(mean_r2)
+    r2_dict_no_tuning[clf] = mean_r2
+    mean_fit_time = np.mean(fit_time)
+    fit_time_dict_no_tuning[clf] = mean_fit_time
+    print("Mean R2 score for %s regressor: %0.2f" % (clf, mean_r2))
+    print("Total fit time for %s regressor: %0.4f seconds" % (clf, mean_fit_time))
+
+    print("-------------x------------------x---------------x---------------")
+    print(" ")
+
+
+NUM_COLORS = 16
+cm = plt.get_cmap('tab20c')
+colors = [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
+
+hyp_tuning_res = list()
+no_hyp_tuning_res = list()
+ctr = 1
+trace_tng_ntng_list = list()
+
+for clf in r2_dict_no_tuning:
+    hyp_tuning_res.append(r2_dict_tuning[clf])
+    no_hyp_tuning_res.append(r2_dict_no_tuning[clf])
+    trace_tng_ntng = go.Scatter(
+        x = [r2_dict_no_tuning[clf]],
+        y = [r2_dict_tuning[clf]],
+        name=clf,
+        mode = 'markers',
+        marker = dict(
+            size = 10,
+            color = 'rgba' + str(colors[ctr]),
+        )
+    )
+    trace_tng_ntng_list.append(trace_tng_ntng)
+    ctr += 1
+
+fontsz = 24
+ao = True
+  
+x_eq_y = list(range( int(min(hyp_tuning_res) - 2), int(max(no_hyp_tuning_res) + 2) ))
+
+trace_x_eq_y = go.Scatter(
+    x = x_eq_y,
+    y = x_eq_y,
+    mode = 'lines',
+    name = 'x = y curve',
+    marker = dict(
+        color = 'rgba' + str(colors[0]),
+    )
+)
+
+trace_tng_ntng_list.append(trace_x_eq_y)
+
+layout_tng_ntng = dict(
+    title = "R2 scores of regressors for tuning and no tuning of hyperparameters",
+    font = dict(family='Times new roman', size=fontsz),
+    yaxis = dict(
+        showgrid=True,
+        showline=True,
+        showticklabels=True,
+        title='Performance using tuned hyperparameters',
+        titlefont=dict(
+            family='Times new roman',
+            size=fontsz
+        )
+    ),
+    xaxis=dict(
+        zeroline=False,
+        showline=True,
+        showticklabels=True,
+        showgrid=True,
+        title='Performance using no hyperparameters',
+        titlefont=dict(
+            family='Times new roman',
+            size=fontsz
+        )
+    ),
+    margin=dict(
+        l=100,
+        r=100,
+        t=100,
+        b=100
+    ),
+    paper_bgcolor='rgb(248, 248, 255)',
+    plot_bgcolor='rgb(248, 248, 255)',
+)
+fig_actual_pred = go.Figure(data=trace_tng_ntng_list, layout=layout_tng_ntng)
+plotly.offline.plot(fig_actual_pred, filename="hyper_performance.html", auto_open=ao)
+pio.write_image(fig_actual_pred, 'plots/hyper_performance.png', width=1200, height=800)    
+
+end_time = time.time()
+print('Total time taken: %d seconds' % int(end_time - start_time))
--- r2_plots.py
+++ r2_plots.py
@@ -25,17 +25,23 @@ start_time = time.time()
 datasets = ["228_elusage.tsv", "485_analcatdata_vehicle.tsv", "523_analcatdata_neavote.tsv", \
             '663_rabe_266.tsv', '687_sleuth_ex1605.tsv']
 
+#datasets = os.listdir("data_used/")
+
+#datasets = ["228_elusage.tsv"]
 
 regressor = Pipeline([
   ('regression', XGBRegressor())
 ])
 
 parameters = [{ \
-    'regression__max_depth': [1, 2, 3, 4, 5], \
-    'regression__n_estimators': [10, 50, 100, 200], \
-    'regression__booster': ['gbtree', 'gblinear', 'dart'], \
+    #'regression__max_depth': [1, 2, 3, 4, 5], \
+    #'regression__n_estimators': [10, 50, 100, 200], \
+    #'regression__booster': ['gbtree', 'gblinear', 'dart'], \
     'regression__random_state': [3111696] \
 }]
+
+fontsz = 24
+ao = False
 
 print('Training started...')
 dataset_accuracies = list()
@@ -46,14 +52,14 @@ for d_set in datasets:
     df = pd.read_csv(data_path, sep="\t")
     label = df["target"].copy()
     data = df.drop("target", axis=1)
-    X_train, X_test, y_train, y_test = model_selection.train_test_split(data, label, test_size=0.25, random_state=3111696)
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(data, label, test_size=0.33, random_state=3111696)
     
     X_train = X_train.values
     X_test = X_test.values
     y_train = y_train.values
     y_test = y_test.values
     
-    optimized_regressor = GridSearchCV(regressor, parameters, cv=5, error_score=0)
+    optimized_regressor = GridSearchCV(regressor, parameters, cv=3, error_score=0)
     
     optimized_regressor.fit(X_train, y_train)
     best_regressor = optimized_regressor.best_estimator_
@@ -66,32 +72,34 @@ for d_set in datasets:
     r2_scores.append(r2_square)
     residual = y_test - prediction
     print("R-square score: %0.2f" % r2_square)
-    
-    '''plt.figure()
-    plt.plot(y_test, color='r')
-    plt.plot(prediction, color='g')
-    plt.grid(True)
-    plt.legend(['True', 'Predicted'])
-    plt.title('Predicted vs true values')
-    plt.show()'''
 
     trace_actual_pred = go.Scatter(
         x = y_test.tolist(),
         y = prediction.tolist(),
         mode = 'markers',
+        name = 'True vs predicted'
+    )
+  
+    x_eq_y = list(range( int(min(y_test) - 1), int(max(y_test) + 1) ))
+
+    trace_x_eq_y = go.Scatter(
+        x = x_eq_y,
+        y = x_eq_y,
+        mode = 'lines',
+        name = 'x = y curve'
     )
 
     layout_actual_pred = dict(
-        title = 'Mean fit time vs R2 regression score',
-        font = dict(family='Times new roman', size=24),
+        title = ('True vs predicted targets (R2: %0.2f)' % r2_square),
+        font = dict(family='Times new roman', size=fontsz),
         yaxis = dict(
             showgrid=True,
             showline=True,
             showticklabels=True,
             title='Predicted targets',
             titlefont=dict(
-            family='Times new roman',
-            size=24
+                family='Times new roman',
+                size=fontsz
             )
         ),
         xaxis=dict(
@@ -102,7 +110,7 @@ for d_set in datasets:
             title='True targets',
             titlefont=dict(
                 family='Times new roman',
-                size=24
+                size=fontsz
             )
         ),
         margin=dict(
@@ -114,23 +122,10 @@ for d_set in datasets:
         paper_bgcolor='rgb(248, 248, 255)',
         plot_bgcolor='rgb(248, 248, 255)',
     )
-
-    fig_actual_pred = go.Figure(data=[trace_actual_pred], layout=layout_actual_pred)
-    plotly.offline.plot(fig_actual_pred, filename="pred_actual.png", auto_open=True)
-    pio.write_image(fig_actual_pred, 'plots/actual_pred.png', width=1200, height=800)
-    
-    '''print("---------------------")
-    
-    plt.figure()
-    plt.plot(prediction, residual, 'ro')
-    max_pred = int(np.max(prediction))
-    min_pred = int(np.min(prediction))
-    plt.plot(np.arange(min_pred, max_pred), [0 for i in range(max_pred - min_pred)], 'b--')
-    plt.grid(True)
-    plt.xlabel('Predicted values')
-    plt.ylabel('Residual (true - predicted) values')
-    plt.title('Residual plot')
-    plt.show()'''
+    fn = "pred_actual_" +  d_set + ".png"
+    fig_actual_pred = go.Figure(data=[trace_actual_pred, trace_x_eq_y], layout=layout_actual_pred)
+    plotly.offline.plot(fig_actual_pred, filename=fn, auto_open=ao)
+    pio.write_image(fig_actual_pred, 'plots/' + fn, width=1200, height=800)
     
     print("Finished dataset: %s" % d_set)
     print("------------------------------------------------------------")
